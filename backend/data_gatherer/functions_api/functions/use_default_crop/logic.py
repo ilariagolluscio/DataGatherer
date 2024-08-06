1c6754622186389c5b9b0ae7fb0a4c147506de83
VicDem <victorgdematteis@gmail.com>
2024-08-06 23:05:51 +0200

    solved bug in isDefault

--- backend/data_gatherer/functions_api/functions/use_default_crop/logic.py
+++ backend/data_gatherer/functions_api/functions/use_default_crop/logic.py
@@ -2,13 +2,18 @@ from storage_api.models.image_models import ImgCrop, Image
 
 
 def apply_default_crop_to_target_image(img_instance: Image, crop_instance: ImgCrop):
+    isDefault = False
+
+    if crop_instance.image.pk == img_instance.pk:
+        isDefault = True
+
     newCrop = ImgCrop(
         fieldName=crop_instance.fieldName,
         topPercent=crop_instance.topPercent,
         leftPercent=crop_instance.leftPercent,
         widthPercent=crop_instance.widthPercent,
         heightPercent=crop_instance.heightPercent,
-        isDefault=False,
+        isDefault=isDefault,
         recognizedText="",
         image=img_instance
     )
