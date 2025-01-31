a83098ca4c29a972eaae0ee757ba1fd37e111a31
VicDem <vi.dematteis02@gmail.com>
2025-01-31 21:18:54 +0100

    updated routes

--- frontend/src/api_router/fx_api.js
+++ frontend/src/api_router/fx_api.js
@@ -2,7 +2,6 @@ import {defaultBaseUrl} from "../global_vars";
 
 export const serverUrl = process.env.REACT_APP_API_URL || defaultBaseUrl
 
-export const uploadUrl = () => serverUrl + "/fx_api/up/upload/"
-export const getMatrixUrl = (prjId) => serverUrl + `/b/fx_api/matrix/${prjId}/`
-export const getBackupFile = () => serverUrl + `/b/fx_api/dump/`
-export const adminUrl = () => serverUrl + '/admin/'
+export const getMatrixUrl = (prjId) => serverUrl + `/a/fx_api/matrix/${prjId}/`
+export const getBackupFile = () => serverUrl + `/a/fx_api/dump/`
+export const adminUrl = () => serverUrl + '/a/admin/'
