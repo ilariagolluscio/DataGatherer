ca95b0a6a6eef37d15e71c3708d91116bb4b3168
VicDem <vi.dematteis02@gmail.com>
2025-01-31 21:20:08 +0100

    minor

--- frontend/src/queries/deleteImage.js
+++ frontend/src/queries/deleteImage.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 import {defaultBaseUrl} from "../global_vars";
 import getAuthAxiosConfig from "./common/getAuthAxiosConfig";
+import Cookies from "universal-cookie";
 
 export const deleteImage = async(id) => {
     const base = process.env.REACT_APP_API_URL || defaultBaseUrl
+
+    const cookies = new Cookies()
+    console.log(cookies.get('csrftoken'))
+
+    const config = await getAuthAxiosConfig()
+    console.log(config)
+
     return await axios.delete(
         base + `/b/storage_api/images/${id}/`,
-        await getAuthAxiosConfig()
+        config
     );
 }
